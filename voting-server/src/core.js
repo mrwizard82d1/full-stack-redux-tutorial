10a58ab96322cce87293d0ee402dfca4e2b2f09a
Larry Jones <larryj@blinds.com>
2017-06-13 16:16:46 -0500

    Write `core.next`

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -2,8 +2,17 @@
  * Created by larryjones on 6/13/17.
  */
 
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 export function setEntries(state, toInclude) {
   return state.set('entries', List(toInclude));
+}
+
+export function next(state) {
+  const entries = state.get('entries');
+  const result = state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+  return result;
 }
