33b41e95709c461e21f8e856556b8bffe7a83211
Larry Jones <larryj@blinds.com>
2017-06-17 16:42:05 -0500

    Report state changes; execute client actions

--- voting-server/index.js
+++ voting-server/index.js
@@ -8,5 +8,6 @@ import startServer from './src/server';
 // Make the store available to consumers of this module
 export const store = makeStore();
 
-// Start the server for clients
-startServer();
+// Start the server for clients passing the store so it can
+// inform interested parties of store changes
+startServer(store);
--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -3,8 +3,32 @@
  */
 
 import Server from 'socket.io';
+import { toJS } from 'immutable';
 
-export default function startServer() {
+export default function startServer(store) {
   // The server listens on port 8090
   const io = new Server().attach(8090);
+  
+  // Subscribe to store changes and broadcast the updated store state (as a JSON object)
+  store.subscribe(() => {
+    io.emit('state', store.getState().toJS())
+  });
+  
+  io.on('connection', (socket) => {
+    // Tell connected clients the current state when
+    // they connect
+    socket.emit('state', store.getState().toJS());
+    
+    // When actions occur on the client, we apply them to the *server* store.
+    socket.on('action', store.dispatch.bind(store));
+  
+    /**
+     * This behavior, applying client events on the server store is *not* standard redux behavior.
+     * However, because the state is just a Javascript object and because actions are just
+     * Javascript objects, it is relatively easy to implement this variant.
+     *
+     * Finally, notice that the technique we use is *incredibly* insecure. We have *not*
+     * authenticated clients and we simply execute the action sent by the client.
+     */
+  });
 }
