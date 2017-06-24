4701c5422e0c11dc508716ddc54f5f843eacd347
Larry Jones <larryj@blinds.com>
2017-06-24 09:29:30 -0500

    Eliminate duplicate test setup code

--- voting-client/src/components/Vote.test.js
+++ voting-client/src/components/Vote.test.js
@@ -6,28 +6,44 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Vote from './Vote';
 
+/**
+ * Make the `props` needed for a specific test.
+ *
+ * The default simply consists of the two entries to vote upon.
+ *
+ * @param addPropsFunction A function to add props to the default result. The
+ * default value of this function is the identity function; that is, it simply
+ * returns the default props.
+ */
+function makeProps(addPropsFunction = r => r) {
+  return addPropsFunction({pair: ['Trainspotting', '28 Days Later']});
+}
+
+function makeCutWithDefaultProps() {
+  const props = makeProps();
+  const cut = shallow(<Vote {...props} />);
+  return cut;
+}
+
 describe('Vote', () => {
   describe('render', () => {
     describe('when not yet voted', () => {
       it('should correctly identify the voting buttons', () => {
-        const props = { pair: ['Trainspotting', '28 Days Later'] };
-        const cut = shallow(<Vote {...props} />);
+        const cut = makeCutWithDefaultProps();
     
         expect(cut.find('button').map(c => c.key()))
           .toEqual(['Trainspotting', '28 Days Later']);
       });
   
       it('should correctly label the voting buttons', () => {
-        const props = { pair: ['Trainspotting', '28 Days Later'] };
-        const cut = shallow(<Vote {...props} />);
+        const cut = makeCutWithDefaultProps();
     
         expect(cut.find('button > h1').map(c => c.text()))
           .toEqual(['Trainspotting', '28 Days Later']);
       });
   
       it('should enable the buttons if no winner', () => {
-        const props = { pair: ['Trainspotting', '28 Days Later'] };
-        const cut = shallow(<Vote {...props} />);
+        const cut = makeCutWithDefaultProps();
     
         expect(cut.filter('button[disabled]').length).toBe(0);
       });
@@ -40,10 +56,11 @@ describe('Vote', () => {
   
   describe('event callbacks', () => {
     it('should respond to clicks on each button', () => {
-      const props = {
-        pair: ['Trainspotting', '28 Days Later'],
-        voteFor: jest.fn()
+      const addVoteFor = (props) => {
+        props.voteFor = jest.fn();
+        return props;
       };
+      const props = makeProps(addVoteFor);
       const cut = shallow(<Vote {...props} />);
   
       ['Trainspotting', '28 Days Later'].forEach(l => {
