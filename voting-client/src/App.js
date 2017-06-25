2c522bdc0b9b647d8e86561b6f410baf5b74e01d
Larry Jones <larryj@blinds.com>
2017-06-25 18:54:17 -0500

    Add `Results` presentational component

--- voting-client/src/App.js
+++ voting-client/src/App.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 /*
  const pair = ['Trainspotting', '28 Days Later'];
@@ -26,6 +27,7 @@ const App = () => (
   <HashRouter>
     <div>
       <Route exact path="/" component={Voting} />
+      <Route path="/results" component={Results} />
     </div>
   </HashRouter>
 );
---
+++ voting-client/src/components/Results.js
@@ -0,0 +1,11 @@
+/**
+ * Created by larryjones on 6/25/17.
+ */
+
+import React from 'react';
+
+const Results = props => (
+  <h1>Hello from Results!</h1>
+);
+
+export default Results;
