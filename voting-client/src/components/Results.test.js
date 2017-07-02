45a79ca417a2badc86da897e70f5cbf9e415350f
Larry Jones <larryj@blinds.com>
2017-07-02 07:42:28 -0500

    Add tests for Results component
    * Present
    * Correct classes of `div's`
    * `h1` elements have correct tetx
    * Renders tallies correctly
    * Renders tally correctly when no votes

---
+++ voting-client/src/components/Results.test.js
@@ -0,0 +1,67 @@
+/**
+ * Created by larryjones on 6/26/17.
+ */
+
+import React from 'react';
+import { List, Map } from 'immutable';
+import { shallow } from 'enzyme';
+import Results from './Results';
+
+describe('Results', () => {
+  it('renders top-level element', () => {
+    const cut = shallow(<Results />);
+    
+    expect(cut.find('div.results')).toBePresent();
+  });
+  
+  it('renders entry `div`s with correct classes', () => {
+    const props = {
+      pair: List.of('Trainspotting', '28 Days Later'),
+    };
+    
+    const cut = shallow(<Results {...props} />);
+    
+    expect(cut.find('div.Trainspotting')).toBePresent();
+    expect(cut.find('div.28.Days.Later')).toBePresent();
+  });
+  
+  it('renders entry `h1`s with correct text', () => {
+    const props = {
+      pair: List.of('Trainspotting', '28 Days Later'),
+    };
+    
+    const cut = shallow(<Results {...props} />);
+    
+    props.pair.forEach(expectedEntry => {
+      expect(cut.findWhere(e => e.name() === 'h1' && e.text() === expectedEntry)).toBePresent();
+    });
+  });
+  
+  it('renders tallies for two items correctly ', () => {
+    const props = {
+      pair: List.of('Trainspotting', '28 Days Later'),
+      tally: Map({'Trainspotting': 5, '28 Days Later': 4}),
+    };
+    
+    const cut = shallow(<Results {...props} />);
+    props.pair.forEach(expectedEntry => {
+      const tallyDiv = cut.findWhere(c => c.name() === 'div' && c.hasClass(expectedEntry));
+      const actualTally = tallyDiv.findWhere(c => c.name() === 'div' && c.hasClass('voteCount'));
+      expect(actualTally.text()).toBe(props.tally.get(expectedEntry).toString());
+    });
+  });
+  
+  it('renders a vote count of 0 if no tally', () =>{
+    const props = {
+      pair: List.of('Trainspotting', '28 Days Later'),
+      tally: Map({'Trainspotting': 4}),
+    };
+  
+    const cut = shallow(<Results {...props} />);
+    const findTallyPredicate = c => c.name() === 'div' && c.hasClass('28 Days Later');
+    const tallyDiv = cut.findWhere(findTallyPredicate);
+    const findVoteCountPredicate = c => c.name() === 'div' && c.hasClass('voteCount');
+    const voteCountDiv = tallyDiv.findWhere(findVoteCountPredicate);
+    expect(voteCountDiv.text()).toBe('0');
+  });
+});
---
+++ voting-client/src/setupTests.js
@@ -0,0 +1,5 @@
+/**
+ * Created by larryjones on 6/26/17.
+ */
+
+import 'jest-enzyme';
