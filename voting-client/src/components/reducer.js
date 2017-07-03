4b56942bac3407703f6c6f950bde4d1335a2524f
Larry Jones <larryj@blinds.com>
2017-07-03 13:49:54 -0500

    Unit test reducer

--- voting-client/src/components/reducer.js
+++ voting-client/src/components/reducer.js
@@ -2,16 +2,20 @@
  * Created by larryjones on 7/2/17.
  */
 
-import { Map, List } from 'immutable';
+/**
+ * immutable@4.x exports a new function, `isImmutable`. However, even though the docs
+ * describe 4.x, the latest code available from npm is `immutable@3.8.1`. To work around
+ * this known issue, posts, for example,
+ * https://stackoverflow.com/questions/31907470/how-to-check-if-object-is-immutable,
+ * suggest using `Iterable.isIterable`.
+ */
+import { Map, Iterable, fromJS } from 'immutable';
 
 export default function reduce(beforeState, action) {
-    return Map({
-      vote: Map({
-        pair: List.of('Trainspotting', '28 Days Later'),
-        tally: Map({
-          'Trainspotting': 5,
-          '28 Days Later': 4,
-        }),
-      }),
-    });
+  switch(action.type) {
+  case 'SET_STATE':
+    return Iterable.isIterable(action.state) ? action.state : fromJS(action.state);
+  default:
+    return Map({});
+  }
 }
--- voting-client/src/components/reducer.test.js
+++ voting-client/src/components/reducer.test.js
@@ -33,8 +33,84 @@ describe('reduce', () => {
           }),
         }),
       }));
-      
     });
-    it('reduces the state if the action has a Javascript payload')
+    
+    it('reduces the state if the action has a Javascript payload', () => {
+      const beforeState = Map({});
+      const action = {
+        type: 'SET_STATE',
+        state: {
+          vote: {
+            pair: ['Trainspotting', '28 Days Later'],
+            tally: {
+              'Trainspotting': 1,
+            },
+          },
+        },
+      };
+      
+      const afterState = reduce(beforeState, action);
+      
+      expect(afterState).toEqual(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1,
+          }),
+        })
+      }));
+    });
+    
+    it('should reduce an initial state of `undefined`', () => {
+      const action = {
+        type: 'SET_STATE',
+        state: Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 5,
+              '28 Days Later': 4,
+            }),
+          }),
+        }),
+      };
+  
+      const afterState = reduce(undefined, action);
+  
+      expect(afterState).toEqual(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 5,
+            '28 Days Later': 4,
+          }),
+        }),
+      }));
+    });
+    
+    it('should reduce an initial state of `undefined`', () => {
+      const action = {
+        type: 'SET_STATE',
+        state: {
+          vote: {
+            pair: ['Trainspotting', '28 Days Later'],
+            tally: {
+              'Trainspotting': 1,
+            },
+          },
+        },
+      };
+    
+      const afterState = reduce(undefined, action);
+    
+      expect(afterState).toEqual(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1,
+          }),
+        }),
+      }));
+    });
   });
 });
