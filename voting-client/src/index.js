41b92ad76df9b1673d45dcc1e390e50fad14b5ad
Larry Jones <larryj@blinds.com>
2017-07-03 21:39:25 -0500

    Add redux store

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import reduce from './components/reducer';
+
+const store = createStore(reduce);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: { 'Sunshine': 2 },
+    },
+  },
+});
 
 ReactDOM.render(
   <HashRouter>
