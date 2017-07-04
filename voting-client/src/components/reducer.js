78640c34a999f1cc440d60885f545206b989b114
Larry Jones <larryj@blinds.com>
2017-07-03 20:26:15 -0500

    Clean up `reducer` (`isImmutable` no longer used)

--- voting-client/src/components/reducer.js
+++ voting-client/src/components/reducer.js
@@ -2,14 +2,7 @@
  * Created by larryjones on 7/2/17.
  */
 
-/**
- * immutable@4.x exports a new function, `isImmutable`. However, even though the docs
- * describe 4.x, the latest code available from npm is `immutable@3.8.1`. To work around
- * this known issue, posts, for example,
- * https://stackoverflow.com/questions/31907470/how-to-check-if-object-is-immutable,
- * suggest using `Iterable.isIterable`.
- */
-import { Map, Iterable, fromJS } from 'immutable';
+import { Map } from 'immutable';
 
 function setState(beforeState, toSetState) {
   return beforeState.merge(toSetState);
@@ -18,7 +11,6 @@ function setState(beforeState, toSetState) {
 export default function reduce(beforeState = Map(), action) {
   switch(action.type) {
   case 'SET_STATE':
-    // return Iterable.isIterable(action.state) ? action.state : fromJS(action.state);
     return setState(beforeState, action.state);
   default:
     return Map({});
